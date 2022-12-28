cfa8030eaa927334bf58f49e542a4d4805a84704
Andreas Renggli <andreas.renggli@ziemergroup.com>
2022-12-28 17:13:37 +0100

    max of merged index should be better than count

--- Batbert/Models/BatButton.cs
+++ Batbert/Models/BatButton.cs
@@ -38,7 +38,7 @@ namespace Batbert.Models
                 if (r.Result == ButtonResult.OK)
                 {
                     ButtonContentList = r.Parameters.GetValue<IEnumerable<IButtonContent>> ("buttonContent").ToList();
-                    ButtonContentCount = ButtonContentList.Count;
+                    ButtonContentCount = ButtonContentList.Max(b => b.MergedIndex);
                 }
             });
         }
